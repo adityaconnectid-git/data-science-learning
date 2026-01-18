d3fa839b2b71a8fb5bc3529639114e0344227cd3
Aditya <aditya.connect.id@gmail.com>
2026-01-18 23:12:41 +0530

    Day 1: Python basics – variables and data types

---
+++ learning_dataScience_Day_01_Pythonbasics–variablesanddatatypes.py
@@ -0,0 +1,49 @@
+a = 1
+b = 2
+c = a+b
+print(c)
+
+a= 30 # variables = container to store a value.
+b= "adi" # keywords = reserved words in python
+c= 71.22 # identifiers = class/function/variable name
+d= None # it is a none type variable
+
+
+# Arithmetic operators
+a = 1
+b = 2
+c = a+b
+print(c)
+# Assignemnt operators 
+a = 7-3 #Assign 7-3 in a
+print(a)
+b=6
+b+3 # Increment the value of b by 3 and then assign it to b
+print(b)
+#Comparison operators
+f=5>3
+print(f)
+g=5!=3
+print(g)
+#logical operators 
+print(not(True))
+
+# Type 
+w = 31
+t=type(w) # class <int>
+print(t)
+kk="33.0"
+ll=float(kk)
+print(type(ll))
+
+#INPUT FUNCTION
+ff=input("Ente your name")
+
+#Questions
+# 1 Write a python program to find remainder when a number is divided by z.
+aa=3
+bb=8
+print(a%b)
+# 2 Check the type of variable assigned using input () function.
+ss=input()
+print(type(ss))
