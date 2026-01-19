6561cf8ce5ba4d8d910429191908c42fad98e618
Aditya <aditya.connect.id@gmail.com>
2026-01-19 18:50:07 +0530

    Refine Day 1 Python basics and fix mistakes

---
+++ Day_01_Pythonbasics–variablesanddatatypes.py
@@ -0,0 +1,37 @@
+# Day 1: Python Basics – Variables, Data Types, Operators
+
+# Variables
+x = 10
+name = "Adi"
+price = 71.22
+value = None
+
+# Arithmetic Operators
+a = 5
+b = 2
+print(a + b)
+print(a % b)
+
+# Assignment Operators
+c = 10
+c += 5
+print(c)
+
+# Comparison Operators
+print(5 > 3)
+print(5 != 3)
+
+# Logical Operators
+print(not True)
+
+# Type Checking
+num = 31
+print(type(num))
+
+s = "33.0"
+f = float(s)
+print(type(f))
+
+# Input Function
+user_name = input("Enter your name: ")
+print(type(user_name))
